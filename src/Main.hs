9b5c506f04e2d164acc4d76de281c7604fd5ee12
Artur Czajka <arturczajka@gmail.com>
2015-09-06 17:15:16 +0200

    Got a basic curses functionality

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import qualified UI.HSCurses.Curses as Curses
+import qualified UI.HSCurses.CursesHelper as CursesH
+import Control.Exception
+
 main :: IO ()
-main = putStrLn "Roguelike0"
+main = do
+    CursesH.start
+    _ <- Curses.cursSet Curses.CursorInvisible
+    run `finally` CursesH.end
+
+run :: IO ()
+run = do
+    Curses.refresh
+    key <- Curses.getCh
+    case key of
+        Curses.KeyBackspace -> return ()
+        Curses.KeyChar ch   -> Curses.wAddStr Curses.stdScr [ch] >> run
+        _                   -> Curses.wAddStr Curses.stdScr "Not a character (Backspace to exit)" >> run
